0c6e217afaa6bb960e862b773ec5895ac13c79fc
Clemence Goh <clemencegoh@users.noreply.github.com>
2020-01-01 00:17:33 +0800

    add some todos for flushbar and adding to recents tab

--- lib/projects/qrCodeScanner/qrFormBLoC.dart
+++ lib/projects/qrCodeScanner/qrFormBLoC.dart
@@ -1,3 +1,4 @@
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/projects/qrCodeScanner/qrGeneratorRepository.dart';
 import 'package:form_bloc/form_bloc.dart';
@@ -33,6 +34,22 @@ class QRFormBloc extends FormBloc<String, String> {
         context: this.context,
         builder: (BuildContext context) => toShow,
       );
+
+      // todo: verify if flushbar works
+      Flushbar(
+        message: "Image saved to recent",
+        icon: Icon(
+          Icons.info_outline,
+          size: 28.0,
+          color: Colors.blue[300],
+        ),
+        margin: EdgeInsets.all(8),
+        borderRadius: 16.0,
+        duration: Duration(seconds: 3),
+        leftBarIndicatorColor: Colors.blue[300],
+      )..show(this.context);
+
+      // todo: actually save this into recents tab
 
       yield currentState.toSuccess();
     } catch (e) {
